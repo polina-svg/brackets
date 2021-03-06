5133d25d50c230428b7098df0f8ef1d0478b706a
Polina <pshibanova00@mail.ru>
2021-03-06 22:28:13 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newbrackConf = [];
+  let string = str;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    newbrackConf.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < newbrackConf.length; j++) {
+      string = string.replace(newbrackConf[j], "");
+    }
+    if (string.length == 0) {
+      return true;
+    }
+  }
+
+  return false;
 }
